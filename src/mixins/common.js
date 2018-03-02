48d053c743750409645675076b110f8cda1e1d8d
mzpbvsig <972403325@qq.com>
2018-03-02 10:53:47 +0800

    add: common mixin

---
+++ src/mixins/common.js
@@ -0,0 +1,38 @@
+import wepy from 'wepy'
+
+export default class Common extends wepy.mixin {
+  data = {
+  }
+
+  props = {
+       phoneticList: {
+           type: Array,
+           default: []
+       },
+       vipPhoneticList: {
+          type: Array,
+          default: []
+       },
+       canUseNumber: {
+          type: Number,
+          default: 9
+       }
+  }
+
+  methods = {
+    playVideo(e){
+        this.$parent.playVideo()
+    },
+    touchStart(e){   
+        this.$parent.touchStart(e)
+    },
+    touchMove(e){
+        this.$parent.touchMove(e)
+    }
+  }
+
+  //非会员切换swiper之后退回上一个
+  back(){
+      this.$invoke("main", "back")
+  }
+}
